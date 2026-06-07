e9741e4a8d9d851531ae2e37c9156d7bc136dfe8
yena4263-glitch <yena4263@gmail.com>
2026-06-08 06:43:55 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -366,7 +366,7 @@ async def handle(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await update.message.reply_text(f"💰 SỐ DƯ: {balance:,} VNĐ")
 
     # ================= ORDERS =================
-        elif (uid in STATE and STATE[uid].get("step") == "link"):
+    elif (uid in STATE and STATE[uid].get("step") == "link"):
         # 1. TRÍCH XUẤT DỮ LIỆU AN TOÀN (Lấy ra biến tạm TRƯỚC KHI xóa)
         # Cách này đảm bảo dữ liệu luôn có để gửi dù sau đó bạn có xóa STATE
         data = STATE.get(uid, {})
